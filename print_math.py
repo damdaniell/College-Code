8a4cf126e2678bd953a7c46bfab450f87a18a699
damdaniell <92399328+damdaniell@users.noreply.github.com>
2023-08-23 11:32:07 -0500

    stuff 3

--- print_math.py
+++ print_math.py
@@ -4,15 +4,10 @@
 #
 # Name: DANIEL ARACKAL
 # Section: 563
-# Assignment: Lab Topic 1 - Activity 1
+# Assignment: Lab Topic 1 - Activity 2
 # Date: 22 08 2023
 #
 #
 # YOUR CODE HERE
 #
 
-#a
-print("Howdy, World!")
-
-#b
-print("I like to play basketball for fun")
