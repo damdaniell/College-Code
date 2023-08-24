602d18cd78f89159804dedbde34dc88c0fcf0b7b
damdaniell <92399328+damdaniell@users.noreply.github.com>
2023-08-24 18:23:03 -0500

    topic 2 activity 3 done

---
+++ Chapter 2/python_code_soup.py
@@ -0,0 +1,123 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: DANIEL ARACKAL
+# Section: 563
+# Assignment: Lab Topic 2 - Activity #3
+# Date: 22 08 2023
+#
+#
+# YOUR CODE HERE
+#
+
+# x = 1
+# y = 10
+# z = 0
+# x = y
+# x += 1
+# y += x
+# y *= x
+# z += x
+# z += y
+# print(z)
+
+# Answer 1
+z = 0
+x = 1
+z += x
+print(z)
+
+# Answer 2
+z = 0
+y = 10
+x = 1
+y += x
+y += x
+x += 1
+y *= x
+x += 1
+y += x
+z += y
+print(z)
+
+# Answer 3
+z = 0
+y = 10
+x = 1
+y += x
+y += x
+y += x
+y += x
+y += x
+y += x
+x += 1
+y *= x
+y += x
+x += 1
+y *= x
+z += y
+print(z)
+
+# Answer 4
+z = 0
+y = 10
+x = 1
+x = y
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+z += y
+print(z)
+
+# Answer 4
+z = 0
+y = 10
+x = 1
+x += 1
+y *= x
+x = y
+y *= x
+y *= x
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+z += x
+z += y
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+print(z)
