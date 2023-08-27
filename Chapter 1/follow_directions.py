836deb9e970c5152080de99b04f4fa7373508d7c
damdaniell <92399328+damdaniell@users.noreply.github.com>
2023-08-27 17:54:48 -0500

    save file

--- Chapter 1/follow_directions.py
+++ Chapter 1/follow_directions.py
@@ -14,14 +14,41 @@
 import math
 
 #array of numbers ranging from 1.1 to 1.00000001
-x = [1.1, 1.01, 1.001, 1.0001, 1.00001, 1.000001, 1.0000001, 1.00000001]
+#x = [1.1, 1.01, 1.001, 1.0001, 1.00001, 1.000001, 1.0000001, 1.00000001]
 
-print("This shows the evalutaion of sin(x-1) / (x-1) evaluated close to x = 1")
-print("My guess is that it will reach 1")
+#print("This shows the evaluation of sin(x-1) / (x-1) evaluated close to x = 1")
+#print("My guess is that it will reach 1")
 
 #for loop
-for y in x:
-    result = (math.sin(y - 1))/(y - 1)
-    print(result)
+#for y in x:
+    #result = (math.sin(y - 1))/(y - 1)
+    #print(result)
+
+#print("My guess was accurate")
+
+y1 = 1.1
+y2 = 1.01
+y3 = 1.001
+y4 = 1.0001
+y5 = 1.00001
+y6 = 1.000001
+y7 = 1.0000001
+y8 = 1.00000001
+
+
+
+
+
+print("This shows the evaluation of sin(x-1) / (x-1) evaluated close to x = 1")
+print("My guess is that it will reach 1")
+
+print((math.sin(y1 - 1))/(y1 - 1))
+print((math.sin(y2 - 1))/(y2 - 1))
+print((math.sin(y3 - 1))/(y3 - 1))
+print((math.sin(y4 - 1))/(y4 - 1))
+print((math.sin(y5 - 1))/(y5 - 1))
+print((math.sin(y6 - 1))/(y6 - 1))
+print((math.sin(y7 - 1))/(y7 - 1))
+print((math.sin(y8 - 1))/(y8 - 1))
 
 print("My guess was accurate")
